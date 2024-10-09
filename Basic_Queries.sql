16552bf47bebf0d101525b2131653b0123b30330
Silessin <118625862+Silessin@users.noreply.github.com>
2024-10-09 22:17:28 +0200

    Create Basic_Queries.sql

---
+++ Basic_Queries.sql
@@ -0,0 +1,31 @@
+-- Basic Queries for the Countries table
+
+-- 1. Select all countries
+SELECT * FROM Countries;
+
+-- 2. Select country names and capitals
+SELECT CountryName, Capital FROM Countries;
+
+-- 3. Find countries in Europe
+SELECT CountryName FROM Countries WHERE Continent = 'Europe';
+
+-- 4. Count the number of countries
+SELECT COUNT(*) FROM Countries;
+
+-- 5. Find countries with a population greater than 50 million
+SELECT CountryName FROM Countries WHERE Population > 50;
+
+-- 6. Get the average population of all countries
+SELECT AVG(Population) FROM Countries;
+
+-- 7. List countries and their currencies
+SELECT CountryName, Currency FROM Countries;
+
+-- 8. Find the country with the highest population
+SELECT CountryName FROM Countries ORDER BY Population DESC LIMIT 1;
+
+-- 9. Group countries by continent and count them
+SELECT Continent, COUNT(*) AS CountryCount FROM Countries GROUP BY Continent;
+
+-- 10. Select countries and their phone codes for countries with 'Dollar' as currency
+SELECT CountryName, PhoneCode FROM Countries WHERE Currency LIKE '%Dollar%';
