c1ef6a5610bece61c1b2d4a979a9ed7c305ddfba
Silessin <118625862+Silessin@users.noreply.github.com>
2024-10-15 10:07:57 +0200

    Update Basic-Queries.sql

--- Basic-Queries.sql
+++ Basic-Queries.sql
@@ -13,13 +13,16 @@ SELECT CountryName FROM Countries WHERE Continent = 'Europe';
 SELECT COUNT(*) FROM Countries;
 
 -- 5. Find countries with a population greater than 50 million
-SELECT CountryName FROM Countries WHERE Population > 50;
+SELECT CountryName, Population FROM Countries WHERE Population > 50;
 
 -- 6. Get the average population of all countries
 SELECT AVG(Population) FROM Countries;
 
--- 7. List countries and their currencies
-SELECT CountryName, Currency FROM Countries;
+-- 7. Get the highest population of all countries
+SELECT MAX(Population) FROM Countries;
 
--- 8. Find the country with the highest population
-SELECT CountryName FROM Countries ORDER BY Population DESC LIMIT 1;
+-- 8. List all the unique currencies of the countries
+SELECT DISTINCT Currency FROM Countries;
+
+-- 9. Find the country with the highest population
+SELECT CountryName, Population FROM Countries ORDER BY Population DESC LIMIT 1;
